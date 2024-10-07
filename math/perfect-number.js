9fe7659c0056ef84a844fe3ad9de4a94ee0c33b0
brijesh <81145474+brijesh704@users.noreply.github.com>
2024-10-07 15:09:34 +0530

    LC-perfect-number [Runtime: 43 ms; Memory: 49 MB]

--- math/perfect-number.js
+++ math/perfect-number.js
@@ -7,9 +7,13 @@ var checkPerfectNumber = function (num) {
     if (num <= 1) return false;
     let count = 0;
     let n = num;
-    for (let i = 1; i < num; i++) {
+    let sq = Math.floor(Math.sqrt(num))
+    for (let i = 1; i <= sq ; i++) {
         if (num % i === 0) {
             count += i;
+            if(i !== 1 &&i !== num/i){
+                count += num/i
+            }
         }
     }
     return  count === n ? true : false;
