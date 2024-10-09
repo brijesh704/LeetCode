85c212a7cd664e16e5283c4ffc461dc391a879b6
brijesh <81145474+brijesh704@users.noreply.github.com>
2024-10-09 18:55:07 +0530

    LC-fibonacci-number [Runtime: 100 ms; Memory: 48.6 MB]

--- memoization/fibonacci-number.js
+++ memoization/fibonacci-number.js
@@ -3,12 +3,15 @@
  * @return {number}
  */
 var fib = function(n) {
+    const memmo = [];
     const f = (n) => {
+        if(n in memmo) return memo[n];
        if( n === 0 ) return 0;
        if(n === 1) return 1;
         let last = f(n-1);
         let slast = f(n-2);
-        return  last + slast;
+        memo =  last + slast;
+        return memo
     }
 
    return f(n)
